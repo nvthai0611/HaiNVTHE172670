39e04b836d917d239cfb6b7d03f6b9fb62695e95
nvthai0611 <nvthai061105@gmail.com>
2024-06-28 11:18:09 +0700

    hainvt update code

--- js/dungchung.js
+++ js/dungchung.js
@@ -606,8 +606,8 @@ function addFooter() {
 
     <!-- ============== Footer ============= -->
     <div class="copy-right">
-        <p><a href="index.html">LDD Phone Store</a> - All rights reserved © 2021 - Designed by
-            <span style="color: #eee; font-weight: bold">group 15th</span></p>
+        <p><a href="index.html">Hải Shop Phone Store</a> - All rights reserved © 2024 - Designed by
+            <span style="color: #eee; font-weight: bold">HaiNVTHE172670@fpt.edu.vn</span></p>
     </div>`);
 }
 
